bdbc0c3e13712211ef60171b6664f39f74f45a9b
eeprom01 <sean.ton@gmail.com>
2017-10-29 16:23:20 -0600

    more addz

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
 var port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static(process.cwd() + "/public"));
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=UPDATE
+app.use(methodOverride("_method"));
 
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-// Import routes and give the server access to them.
+// Import routes and access
 var routes = require("./controllers/burgers_controller");
 app.use("/", routes);
-app.listen(port);
-
-// app.xxx('/', function(req, res){
-//     app.render('burgers');
-// })
+app.listen(port, function() {
+    console.log("Listening on PORT " + port);
+  });
 
 
 
