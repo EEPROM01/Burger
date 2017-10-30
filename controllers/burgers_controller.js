c7bdc88bebba522babc2b4efc30ffab1a5423922
eeprom01 <sean.ton@gmail.com>
2017-10-29 20:42:12 -0600

    klal

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 // Import the model 
-var burger = require("/models/burger.js");
+var burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
