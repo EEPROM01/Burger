b7fcdb7ba6fbaeebb0f81fcf0921630476c26a92
eeprom01 <sean.ton@gmail.com>
2017-10-29 20:18:20 -0600

    getting late

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,10 @@
 var mysql = require("mysql"); 
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "19ZoSo99",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
 if (process.env.JAWS_URL){
   connection = mysql.createConnection(process.env.JAWS_URL);
 }else{
   connection= mysql.createConnection({
+    port: 3306,
     host: "localhost",
     user: "root",
     password: "19ZoSo99",
